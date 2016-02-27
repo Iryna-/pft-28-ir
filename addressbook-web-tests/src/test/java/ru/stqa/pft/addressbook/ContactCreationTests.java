a120a484ac95ade8eb519b32d3c822bf081a8f27
Iryna- <isvitlychenko@gmail.com>
2016-02-27 11:15:44 +0000

    Added ContactCreationTest that extends TestBase

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class ContactCreationTests extends TestBase {
+
+    
+    @Test
+    public void ContactCreationTests() {
+        wd.findElement(By.linkText("add new")).click();
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys("Name");
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys("Surname");
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys("name.surname@gmail.com");
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys("07511111111");
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys("Middle");
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("address2")).click();
+        wd.findElement(By.name("address2")).clear();
+        wd.findElement(By.name("address2")).sendKeys("Address line 1");
+        wd.findElement(By.name("submit")).click();
+        wd.findElement(By.linkText("home")).click();
+    }
+}
