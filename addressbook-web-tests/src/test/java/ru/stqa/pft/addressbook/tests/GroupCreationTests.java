29fa0cf47e7737d4609c6769851a2483465b03e8
Iryna- <isvitlychenko@gmail.com>
2016-03-25 21:32:40 +0000

    Reading test data for GroupCreationTest from the .csv file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,27 +18,23 @@ import static org.testng.Assert.assertEquals;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups (){
+  public Iterator<Object[]> validGroups () throws IOException {
 
     //Filling out a list of arrays.
     // Each array contains a set of data for one launch of the test method
 
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new GroupData()
-                              .withName("group1")
-                              .withHeader("header1")
-                              .withFooter("footer1")});
-    list.add(new Object[]{new GroupData()
-                              .withName("group2")
-                              .withHeader("header2")
-                              .withFooter("footer2")});
-    list.add(new Object[]{new GroupData()
-                              .withName("group3")
-                              .withHeader("header3")
-                              .withFooter("footer3")});
-
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.scv"));
+    String line = reader.readLine();
+    while (line !=null){
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData()
+                  .withName(split[0])
+                  .withHeader(split[1])
+                  .withFooter(split[2])});
+      line = reader.readLine();
+    }
     //Test framework uses the iterator to extract each array and use it for the test
-
     return list.iterator();
   }
 
