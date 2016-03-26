d8bd0b4442f6c1c3f41709f0784363536a2eba39
Iryna- <isvitlychenko@gmail.com>
2016-03-26 15:23:57 +0000

    Close writers and readers automatically

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -56,38 +56,40 @@ public class ContactDataGenerator {
   }
 
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
-    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    Gson gson = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .excludeFieldsWithoutExposeAnnotation()
+                    .create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)){
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file);){
+      writer.write(xml);
+    }
   }
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    //creating all data in cache
-    for (ContactData contact : contacts){
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n",
-              contact.getName(),
-              contact.getMiddleName(),
-              contact.getSurname(),
-              contact.getEmail(),
-              contact.getHomePhone(),
-              contact.getAddress(),
-              contact.getGroup(),
-              contact.getPhoto()));
+    try (Writer writer = new FileWriter(file)){
+      //creating all data in cache
+      for (ContactData contact : contacts){
+        writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n",
+                contact.getName(),
+                contact.getMiddleName(),
+                contact.getSurname(),
+                contact.getEmail(),
+                contact.getHomePhone(),
+                contact.getAddress(),
+                contact.getGroup(),
+                contact.getPhoto()));
+      }
     }
-    //saving all cached data to the disk
-    writer.close();
   }
 
   private List<ContactData> generateContacts(int count) {
