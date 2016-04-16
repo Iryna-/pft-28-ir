f3af64216af452373f24e87be905175315379ce7
Iryna- <isvitlychenko@gmail.com>
2016-04-16 23:04:35 +0100

    Added a step to conditionally skip the test if a specified bug in Bufigy is open (not "closed").

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -1,13 +1,15 @@
 package ru.stqa.pft.rest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.jayway.restassured.RestAssured;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 import java.util.Set;
 import static org.testng.Assert.assertEquals;
 
@@ -20,6 +22,9 @@ public class RestAssuredTests {
 
   @Test
   public void testCreateIssue () throws IOException {
+
+    skipIfNotFixed(8);
+
     Set<Issue> oldIssues = getIssues();
     Issue newIssue = new Issue().withSubject("Test subject").withDescription("Test description");
     int issueId = createIssue(newIssue);
@@ -45,5 +50,24 @@ public class RestAssuredTests {
 
     JsonElement parsed = new JsonParser().parse(json);
     return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+
+
+  public void skipIfNotFixed(int issueId) {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+
+  public boolean isIssueOpen (int issueId) {
+    String json = RestAssured.get(String.format("http://demo.bugify.com/api/issues/%s.json", issueId)).asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonArray issues = parsed.getAsJsonObject().getAsJsonArray("issues");
+    String status = issues.get(0).getAsJsonObject().get("state_name").getAsString();
+    if (status.equals("Closed")){
+      return false;
+    }
+    return true;
   }
 }
