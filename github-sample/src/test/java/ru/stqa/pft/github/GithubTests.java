18437e467cb713e0110417b9aff3b857731210fb
Iryna- <isvitlychenko@gmail.com>
2016-04-17 22:32:06 +0100

    Amended GitHub API tests to show the commit message instead of number

--- github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -4,15 +4,17 @@ import com.google.common.collect.ImmutableMap;
 import com.jcabi.github.*;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class GithubTests {
 
   @Test
 
-  public void testCommits (){
-    Github github = new RtGithub("ff8ee3952c23a47ca7a3bdab74e71e11c779613c");
+  public void testCommits () throws IOException {
+    Github github = new RtGithub("cd4664c180d53716494e9b61d3084a729db6fa81");
     RepoCommits commits = github.repos().get(new Coordinates.Simple("Iryna-", "pft-28-iryna.svitlychenko")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
-      System.out.println(commit);
+      System.out.println(new RepoCommit.Smart(commit).message());
     }
   }
 }
